7e0b30601c3ec7e801aee7e219a432295ec35ef3
wlazlopwl <wlazlopwl@gmail.com>
2019-05-29 10:12:42 +0200

    edit menu

--- app/src/main/java/com/example/pawel/myapp/MainActivity.java
+++ app/src/main/java/com/example/pawel/myapp/MainActivity.java
@@ -106,15 +106,16 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
+        }  else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_setting) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_help) {
+
+        }
+        else if (id==R.id.nav_logout){
 
         }
 
